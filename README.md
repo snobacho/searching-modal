a15c48121ede895428bb195c95836ac92e80a119
snobacho <giorgicheli@gmail.com>
2023-03-18 14:06:31 +0400

    change search functionality and add error msg and README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a search modal for the Sciencelib wordpress website
--- script.js
+++ script.js
@@ -1,46 +1,59 @@
-  const searchModal = document.getElementById("search-modal"),
-        openModalBtn = document.getElementById("open-modal-btn"),
-        closeModal = document.getElementById("close-modal");
-        searchBtn = document.getElementById("search-btn");
+const searchModal = document.getElementById("search-modal"),
+      openModalBtn = document.getElementById("open-modal-btn"),
+      closeModal = document.getElementById("close-modal");
+      searchBtn = document.getElementById("search-btn");
 
-  openModalBtn.onclick = () => {
-    searchModal.style.display = "block";
-  }
-  closeModal.onclick = () => {
-    searchModal.style.display = "none";
-  }
-  searchBtn.onclick = () => {
+openModalBtn.onclick = () => {
+  searchModal.style.display = "block";
+}
+closeModal.onclick = () => {
+  searchModal.style.display = "none";
+}
+searchBtn.onclick = () => {
+  const link = getLink();
+  if (link) {
     searchModal.style.display ="none";
+    window.open(link);
   }
+}
 // end modal window
 
 
-  
-  const searchInput = document.getElementById('search-input');
-  const radioBtns = document.querySelectorAll('input[type=radio][name=search]');
+const searchInput = document.getElementById('search-input');
+const radioBtns = document.querySelectorAll('input[type=radio][name=search]');
 
-  function getLink() {
+function getLink() {
+  const inputValue = searchInput.value.trim();
+  if (inputValue !== '') {
     const selectedSearch = [...radioBtns].find(rb => rb.checked).value;
     const baseUrls = {
       opac: 'https://opac.sciencelib.ge/cgi-bin/koha/opac-search.pl?q=',
       openlibrary: 'http://openlibrary.ge/simple-search?query=',
       openscience: 'https://openscience.ge/simple-search?query=',
       openjournals: 'https://openjournals.ge/index.php/index/search/?query='
-      };
-      return baseUrls[selectedSearch] + searchInput.value;
-    }
-    
-  
-    searchBtn.addEventListener('click', () => {
-      const link = getLink();
+    };
+    return baseUrls[selectedSearch] + inputValue;
+  }
+  const errorMsg = document.getElementById('error-msg')
+  errorMsg.style.opacity = '1'
+  setTimeout(() => {
+    errorMsg.style.opacity = "0";
+  }, 3000); 
+  return null;
+}
+
+searchBtn.addEventListener('click', () => {
+  const link = getLink();
+  if (link) {
+    window.open(link);
+  }
+});
+
+searchInput.addEventListener('keyup', event => {
+  if (event.key === 'Enter') {
+    const link = getLink();
+    if (link) {
       window.open(link);
-    });
-
-    searchInput.addEventListener('keyup', event => {
-      if (event.key === 'Enter') {
-        const link = getLink();
-        window.open(link);
-      }
-  });
-
-   
+    }
+  }
+});
